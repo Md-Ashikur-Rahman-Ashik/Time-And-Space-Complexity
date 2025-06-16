1005746a84e0553bc4da6d2c4bd1b106a689d257
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-16 23:11:41 +0600

    Exploring Quadratic complexity

---
+++ quadraticComplexity.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    for (int i = 0; i < num; i++)
+    {
+        cout << "Hello" << endl;
+
+        for (int j = 0; j < num; j++)
+        {
+            cout << "Hello" << endl;
+        }
+    }
+
+    return 0;
+}
