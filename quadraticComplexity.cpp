90262d2bdaa6c583445d3b96594ab68b5bf3da25
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-18 23:20:37 +0600

    Explored quadratic complexity

--- quadraticComplexity.cpp
+++ quadraticComplexity.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-    int num;
-    cin >> num;
+    int num1, num2;
+    cin >> num1 >> num2;
 
-    for (int i = 0; i < num; i++)
+    for (int i = 0; i < num1; i++)
     {
         cout << "Hello" << endl;
 
-        for (int j = 0; j < num; j++)
+        for (int j = 0; j < num2; j++)
         {
             cout << "Hello" << endl;
         }
