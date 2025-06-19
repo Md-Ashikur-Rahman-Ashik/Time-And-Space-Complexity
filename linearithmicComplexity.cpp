b3460354256e150efafcbd0c8197d93cd5093118
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-19 12:30:06 +0600

    Explored linearithmic complexity

---
+++ linearithmicComplexity.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    for (int i = 1; i <= num; i++)
+    {
+        for (int j = 1; j <= num; j * 2)
+        {
+            cout << "Hello World" << endl;
+        }
+    }
+
+    return 0;
+}
